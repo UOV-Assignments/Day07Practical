1ab20cf58eb81c1b9c2940010a5571981bc00e6e
Lasanga Dissanayaka <129869155+lasanga890@users.noreply.github.com>
2024-11-23 07:31:31 +0000

    create a state for store child name wich send from child component

--- day07/src/App.js
+++ day07/src/App.js
@@ -1,10 +1,12 @@
-
+import {useState} from 'react'
 import './App.css';
 import ChildComp from './ChildComp';
 
 function App() {
+  const [childName,setChildName] = useState("Default");
   return (
     <div className="App">
+      <h1>Hello I'm Parent</h1>
       <ChildComp />
     </div>
   );
