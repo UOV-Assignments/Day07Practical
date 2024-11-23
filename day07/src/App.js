049acb794453e1488460792d816576405db4cd9b
Lasanga Dissanayaka <129869155+lasanga890@users.noreply.github.com>
2024-11-23 07:37:38 +0000

    adding some comments to understand codebase

--- day07/src/App.js
+++ day07/src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import ChildComp from './ChildComp';
 
 function App() {
+  //state for store child component message
   const [childName,setChildName] = useState("Default");
+
+  //function to set child component message
   const receiveName = (name=>{
     setChildName(name);
   })
   return (
     <div className="App">
       <h1>Hello I'm Parent</h1>
+      {/* Call the Child Component */}
       <ChildComp setFunc={receiveName} />
+      {/* Display the child Component message */}
       <p>Parent : My Child said{childName}</p>
     </div>
   );
--- day07/src/ChildComp.js
+++ day07/src/ChildComp.js
@@ -2,6 +2,7 @@ export default function ChildComp(props){
     return(
         <>
         <h3>Hello I'm Child</h3>
+        {/* Send message to parent component */}
         {
             props.setFunc(`Hello I'm Lasanga`)
         }
